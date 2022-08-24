cd27eada840b495f075a923710a26c176684ac54
T-Lind <tiernanxklindauer@gmail.com>
2022-08-24 14:00:32 -0500

    RN not working :(

--- ReinforcementLearning/CartPoleRN.py
+++ ReinforcementLearning/CartPoleRN.py
@@ -3,6 +3,10 @@ import keyboard
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    datefmt='%m/%d%Y %H: %M: %S')
 
 # Load the CartPole game from OpenAIGym
 env = gym.make('CartPole-v1')
@@ -16,23 +20,70 @@ model = keras.models.Sequential([
     keras.layers.Dense(1, activation='sigmoid'),
 ])
 
+def play_one_step(env, obs, model, loss_fn):
+    with tf.GradientTape() as tape:
+        left_proba = model(obs[np.newaxis])
+        action = (tf.random.uniform([1, 1]) > left_proba)
+        y_target = tf.constant([[1.]]) - tf.cast(action, tf.float32)
+        loss = tf.reduce_mean(loss_fn(y_target, left_proba))
+    grads = tape.gradient(loss, model.trainable_variables)
+    obs, reward, done, _ = env.step(int(action[0, 0].numpy()))
+    return obs, reward, done, grads
 
+def play_multiple_episodes(env, n_episodes, n_max_steps, model, loss_fn):
+    all_rewards = []
+    all_grads = []
+    for episode in range(n_episodes):
+        current_rewards = []
+        current_grads = []
+        obs = env.reset()
+        for step in range(n_max_steps):
+            obs, reward, done, grads = play_one_step(env, obs, model, loss_fn)
+            current_rewards.append(reward)
+            current_grads.append(grads)
+            if done:
+                break
+        all_rewards.append(current_rewards)
+        all_grads.append(current_grads)
+    return all_rewards, all_grads
 
+def discount_rewards(rewards, discount_factor):
+    discounted = np.array(rewards)
+    for step in range(len(rewards) - 2, -1 , 1):
+        discounted[step] += discounted[step+1]*discount_factor
+    return discounted
 
-totals = []
-for episode in range(500):
-    episode_rewards = 0
-    obs = env.reset()
-    for step in range(200):
-        action = basic_policy(obs)
-        obs, reward, done, info = env.step(action)
-        episode_rewards += reward
-        if done:
-            break
-        if keyboard.is_pressed('q'):
-            break
-    if keyboard.is_pressed('q'):
-        break
-    totals.append(episode_rewards)
+def discount_and_normalize_rewards(all_rewards, discount_factor):
+    all_discounted_rewards = [discount_rewards(rewards, discount_factor) for rewards in all_rewards]
+    flat_rewards = np.concatenate(all_discounted_rewards)
+    reward_mean = flat_rewards.mean()
+    reward_std = flat_rewards.std()
 
-print(f"Max steps: {np.max(totals)}, Min steps: {np.min(totals)}, Std: {np.std(totals)}, Average: {np.mean(totals)}")
+    return [(discounted_rewards - reward_mean) / reward_std for discounted_rewards in all_rewards]
+
+n_iterations = 150
+n_episodes_per_update = 10
+n_max_steps = 200
+discount_factor = 0.95
+
+optimizer = keras.optimizers.Adam(lr=0.1)
+loss_fn = keras.losses.binary_crossentropy
+
+for iteration in range(n_iterations):
+    all_rewards, all_grads = play_multiple_episodes(
+        env, n_episodes_per_update, n_max_steps, model, loss_fn)
+    total_rewards = sum(map(sum, all_rewards))                     # Not shown in the book
+    print("\rIteration: {}, mean rewards: {:.1f}".format(          # Not shown
+        iteration, total_rewards / n_episodes_per_update), end="") # Not shown
+    all_final_rewards = discount_and_normalize_rewards(all_rewards, discount_factor)
+
+    all_mean_grads = []
+    for var_index in range(len(model.trainable_variables)):
+        mean_grads = tf.reduce_mean(
+            [final_reward * all_grads[episode_index][step][var_index]
+             for episode_index, final_rewards in enumerate(all_final_rewards)
+                for step, final_reward in enumerate(final_rewards)], axis=0)
+        all_mean_grads.append(mean_grads)
+    optimizer.apply_gradients(zip(all_mean_grads, model.trainable_variables))
+
+env.close()
