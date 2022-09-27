52d13bfdb87a59724228716b06363ad7f216c4c2
T-Lind <tiernanxklindauer@gmail.com>
2022-09-26 20:18:59 -0500

    render env

--- ftc_field/envs/ftc_field_env.py
+++ ftc_field/envs/ftc_field_env.py
@@ -141,7 +141,6 @@ class FieldEnvFTC(gym.Env):
         self._pole_states[2][3].pole_type = "high"
         self._pole_states[3][2].pole_type = "high"
 
-
     def reset(self, seed=None, options=None):
         super().reset(seed=seed)
 
@@ -154,7 +153,6 @@ class FieldEnvFTC(gym.Env):
 
         self._pole_states = [[PoleState()] * 5] * 5
         self._set_pole_states()
-
 
         self._corner_states = [False] * 4
 
@@ -296,13 +294,74 @@ class FieldEnvFTC(gym.Env):
         if self.render_mode == "human":
             self._render_frame()
 
+        self._step_count += 1
+
         # Important: returns the rewards as a tuple with red reward first
         return observation, (reward_red, reward_blue), terminated, info
-
-
-
-        self._step_count += 1
 
     def render(self):
         if self.render_mode == "rgb_array":
             return self._render_frame()
+
+    def _render_frame(self):
+        if self.window is None and self.render_mode == "human":
+            pygame.init()
+            pygame.display.init()
+            self.window = pygame.display.set_mode((self.window_size, self.window_size))
+        if self.clock is None and self.render_mode == "human":
+            self.clock = pygame.time.Clock()
+
+        canvas = pygame.Surface((self.window_size, self.window_size))
+        canvas.fill((255, 255, 255))
+        pix_square_size = (self.window_size / self.size)  # The size of a single grid square in pixels
+
+        # Draw agent red
+        pygame.draw.rect(
+            canvas,
+            (255, 0, 0),
+            pygame.Rect(
+                pix_square_size * self._agent_red_location,
+                (pix_square_size, pix_square_size),
+            ),
+        )
+
+        # Draw agent blue
+        pygame.draw.rect(
+            canvas,
+            (0, 0, 255),
+            pygame.Rect(
+                pix_square_size * self._agent_red_location,
+                (pix_square_size, pix_square_size),
+            ),
+        )
+
+        # Add grid lines
+        for x in range(self.size + 1):
+            pygame.draw.line(
+                canvas,
+                0,
+                (0, pix_square_size * x),
+                (self.window_size, pix_square_size * x),
+                width=3,
+            )
+            pygame.draw.line(
+                canvas,
+                0,
+                (pix_square_size * x, 0),
+                (pix_square_size * x, self.window_size),
+                width=3,
+            )
+
+        if self.render_mode == "human":
+            # The following line copies our drawings from `canvas` to the visible window
+            self.window.blit(canvas, canvas.get_rect())
+            pygame.event.pump()
+            pygame.display.update()
+
+            # We need to ensure that human-rendering occurs at the predefined framerate.
+            # The following line will automatically add a delay to keep the framerate stable.
+            self.clock.tick(self.metadata["render_fps"])
+        else:  # rgb_array
+            return np.transpose(
+                np.array(pygame.surfarray.pixels3d(canvas)), axes=(1, 0, 2)
+            )
