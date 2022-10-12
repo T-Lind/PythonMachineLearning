fc4b18d6636bd5845e9bfc1a78842b8befbaea94
T-Lind <tiernanxklindauer@gmail.com>
2022-10-11 20:39:22 -0500

    repnet procedural testing

--- ReinforcementLearning/CartPoleActorCritic.py
+++ ReinforcementLearning/CartPoleActorCritic.py
@@ -4,73 +4,66 @@ import numpy as np
 import statistics
 import tensorflow as tf
 import tqdm
-from actor_critic import train_step, ActorCritic, create_env
+from actor_critic import ActorCritic, CartPoleEnv
 import matplotlib.pyplot as plt
 
 # Create the environment
-print(gym)
-env = create_env()
+def train_rl():
+    cartpole = CartPoleEnv()
 
-# Set seed for experiment reproducibility
-# seed = 42
-# env.seed(seed)
-# tf.random.set_seed(seed)
-# np.random.seed(seed)
+    # Small epsilon value for stabilizing division operations
+    eps = np.finfo(np.float32).eps.item()
 
-# Small epsilon value for stabilizing division operations
-eps = np.finfo(np.float32).eps.item()
+    min_episodes_criterion = 100
+    max_episodes = 10000
+    max_steps_per_episode = 1000
 
-min_episodes_criterion = 100
-max_episodes = 10000
-max_steps_per_episode = 1000
+    # Cartpole-v0 is considered solved if average reward is >= 195 over 100
+    # consecutive trials
+    reward_threshold = 195
+    running_reward = 0
 
-# Cartpole-v0 is considered solved if average reward is >= 195 over 100
-# consecutive trials
-reward_threshold = 195
-running_reward = 0
+    # Discount factor for future rewards
+    gamma = 0.99
 
-# Discount factor for future rewards
-gamma = 0.99
+    # Keep last episodes reward
+    episodes_reward: collections.deque = collections.deque(maxlen=min_episodes_criterion)
 
-# Keep last episodes reward
-episodes_reward: collections.deque = collections.deque(maxlen=min_episodes_criterion)
+    num_actions = cartpole.env.action_space.n  # 2
+    num_hidden_units = 128
 
-num_actions = env.action_space.n  # 2
-num_hidden_units = 128
+    model = ActorCritic(num_actions, num_hidden_units)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
 
-model = ActorCritic(num_actions, num_hidden_units)
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
+    performance_list = []
 
-performance_list = []
+    iters = 0
 
-iters = 0
+    with tqdm.trange(max_episodes) as t:
+        for i in t:
+            initial_state = tf.constant(cartpole.env.reset(), dtype=tf.float32)
+            episode_reward = int(cartpole.train_step(
+                initial_state, model, optimizer, gamma, max_steps_per_episode))
 
-with tqdm.trange(max_episodes) as t:
-    for i in t:
-        initial_state = tf.constant(env.reset(), dtype=tf.float32)
-        episode_reward = int(train_step(
-            initial_state, model, optimizer, gamma, max_steps_per_episode))
+            episodes_reward.append(episode_reward)
+            running_reward = statistics.mean(episodes_reward)
 
-        episodes_reward.append(episode_reward)
-        running_reward = statistics.mean(episodes_reward)
+            iters += 1
 
-        iters += 1
+            performance_list.append(running_reward)
 
-        performance_list.append(running_reward)
+            t.set_description(f'Episode {i}')
+            t.set_postfix(
+                episode_reward=episode_reward, running_reward=running_reward)
 
-        t.set_description(f'Episode {i}')
-        t.set_postfix(
-            episode_reward=episode_reward, running_reward=running_reward)
+            # Show average episode reward every 10 episodes
+            if i % 10 == 0:
+                pass  # print(f'Episode {i}: average reward: {avg_reward}')
 
-        # Show average episode reward every 10 episodes
-        if i % 10 == 0:
-            pass  # print(f'Episode {i}: average reward: {avg_reward}')
+            if running_reward > reward_threshold and i >= min_episodes_criterion:
+                break
+    return performance_list
 
-        if running_reward > reward_threshold and i >= min_episodes_criterion:
-            break
+plt.plot(train_rl())
 
-print(f'\nSolved at episode {i}: average reward: {running_reward:.2f}!')
-print(performance_list)
-plt.title("Normal RL Actor-Critic running reward with each iteration (TLind):")
-plt.plot(performance_list)
 plt.show()
--- repnetwork/CompareRepnetProcedural.py
+++ repnetwork/CompareRepnetProcedural.py
@@ -13,6 +13,7 @@ num_networks_trained = 5
 
 reward_threshold = 195
 
+
 def train_repnet():
     cartpole = CartPoleEnv()
 
@@ -83,8 +84,64 @@ def get_repnet_data():
         result = train_repnet()
     return result
 
-result = get_repnet_data()
-plt.plot(result)
-result = get_repnet_data()
-plt.plot(result)
+
+def train_rl():
+    cartpole = CartPoleEnv()
+
+    # Small epsilon value for stabilizing division operations
+    eps = np.finfo(np.float32).eps.item()
+
+    min_episodes_criterion = 100
+    max_episodes = 10000
+    max_steps_per_episode = 1000
+
+    # Cartpole-v0 is considered solved if average reward is >= 195 over 100
+    # consecutive trials
+    running_reward = 0
+
+    # Discount factor for future rewards
+    gamma = 0.99
+
+    # Keep last episodes reward
+    episodes_reward: collections.deque = collections.deque(maxlen=min_episodes_criterion)
+
+    num_actions = cartpole.env.action_space.n  # 2
+    num_hidden_units = 128
+
+    model = ActorCritic(num_actions, num_hidden_units)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
+
+    performance_list = []
+
+    iters = 0
+
+    with tqdm.trange(max_episodes) as t:
+        for i in t:
+            initial_state = tf.constant(cartpole.env.reset(), dtype=tf.float32)
+            episode_reward = int(cartpole.train_step(
+                initial_state, model, optimizer, gamma, max_steps_per_episode))
+
+            episodes_reward.append(episode_reward)
+            running_reward = statistics.mean(episodes_reward)
+
+            iters += 1
+
+            performance_list.append(running_reward)
+
+            t.set_description(f'Episode {i}')
+            t.set_postfix(
+                episode_reward=episode_reward, running_reward=running_reward)
+
+            # Show average episode reward every 10 episodes
+            if i % 10 == 0:
+                pass  # print(f'Episode {i}: average reward: {avg_reward}')
+
+            if running_reward > reward_threshold and i >= min_episodes_criterion:
+                break
+    return performance_list
+
+
+for _ in range(50):
+    plt.plot(get_repnet_data(), color="magenta")
+    plt.plot(get_repnet_data(), color="teal")
 plt.show()
