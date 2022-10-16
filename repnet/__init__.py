4d3c96da3f91957ffbec4b8c5e6dd2a3ef284e2b
T-Lind <tiernanxklindauer@gmail.com>
2022-10-16 17:04:33 -0500

    Documentation and kwargs for training functions

--- repnet/__init__.py
+++ repnet/__init__.py
@@ -1,4 +1,4 @@
 from repnet.RepTree import Branch
 from repnet.RepNet import RepNet
 from repnet.CartPoleExample import train_one_repnet_model
-from repnet.RetrieveData import train_repnet, train_rl_hardstop, train_rl
+from repnet.RetrieveDataCartPole import train_repnet, train_rl, train_distributed_repnet
