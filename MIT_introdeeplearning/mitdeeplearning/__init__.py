67b47ef2099f78c1821533c19f923bb729d04ba8
T-Lind <tiernanxklindauer@gmail.com>
2022-11-07 11:05:38 -0600

    MIT deep learning repo

---
+++ MIT_introdeeplearning/mitdeeplearning/__init__.py
@@ -0,0 +1,4 @@
+import MIT_introdeeplearning.mitdeeplearning.util
+import MIT_introdeeplearning.mitdeeplearning.lab1
+import MIT_introdeeplearning.mitdeeplearning.lab2
+import MIT_introdeeplearning.mitdeeplearning.lab3
