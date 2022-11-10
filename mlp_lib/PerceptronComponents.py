ec4b75598a4a67ce9e50291a9405df372ce4475e
T-Lind <tiernanxklindauer@gmail.com>
2022-11-10 10:40:33 -0600

    mlp code

--- mlp_lib/PerceptronComponents.py
+++ mlp_lib/PerceptronComponents.py
@@ -51,14 +51,3 @@ class Network:
             else:
                 result = self.layers[i](result)
         return result
-
-
-# Example network
-network = Network([
-    Input(3, 3),
-    Dense(3, 5, activation_func=activation("leaky_relu")),
-    Dense(5, 128, activation_func=activation("sigmoid")),
-    Dense(128, 2, activation_func=activation("sigmoid")),
-    Dense(2, 1, activation_func=activation("relu")),
-])
-print(network.predict([3, 2, 1]))
